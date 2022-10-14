f15d9d094fd81dda9e5de58fc7aa41cf6be1f278
Leonard Yu <hyu@xenanetworks.com>
2022-10-14 23:58:16 +0200

    build: Fix dependency version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def main():
         url="https://github.com/xenanetworks/open-automation-config-converter",
         packages=setuptools.find_packages(),
         license='Apache 2.0',
-        install_requires=["pydantic==1.8.2", "datamodel-code-generator==0.13.0"],
+        install_requires=["pydantic", "datamodel-code-generator"],
         classifiers=[
             "Development Status :: 5 - Production/Stable",
             "Intended Audience :: Developers",

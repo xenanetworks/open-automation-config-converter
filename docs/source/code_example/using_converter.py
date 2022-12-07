1c7d49cbc45423f97b3e35bbb7e445d18cf1e43a
Leonard Yu <hyu@xenanetworks.com>
2022-12-07 16:18:56 +0100

    docs: Improve the example of using converter

--- docs/source/code_example/using_converter.py
+++ docs/source/code_example/using_converter.py
@@ -4,22 +4,34 @@ from xoa_core import controller
 from xoa_converter.entry import converter
 from xoa_converter.types import TestSuiteType
 
+# source config file to be converted
+OLD_CONFIG_FILE = "my_old2544_config.v2544" 
+NEW_CONFIG_FILE = "xoa2544.json"
+T_SUITE_NAME = "RFC-2544"
+
 async def start():
-    SOURCE_CONFIG_FILE = "my_old2544_config.v2544" # source config file to be converted
 
-    core_ctrl = await controller.MainController() # create an instance of xoa core controller
-    info = core_ctrl.get_test_suite_info("RFC-2544") # get 2544 test suite information from the core's registration
-    target_schema = json.load(info['schema']) # get the target json schema
+    # create an instance of xoa core controller
+    c = await controller.MainController()
 
-    with open(SOURCE_CONFIG_FILE, 'r') as source_data_file:
-        target_config = converter(
-            test_suite_type=TestSuiteType.RFC2544, 
-            source_config=source_data_file.read(), 
-            target_schema=target_schema
-        )
+    with open(OLD_CONFIG_FILE) as f:
+        app_data = f.read()
+
+        # get 2544 test suite information from the core's registration
+        info = c.get_test_suite_info(T_SUITE_NAME)
+        if not info:
+            print("Test suite is not recognized.")
+            return None
         
-        print(target_config)
+        # convert the old config file into new config file
+        new_data = converter(TestSuiteType.RFC2544, app_data, info["schema"])
 
+        with open(NEW_CONFIG_FILE, "w") as f:
+            f.write(new_data)
+        
+        # you can use the config file below to start the test
+        config = json.loads(new_data)
+        exec_id = c.start_test_suite(T_SUITE_NAME, config, debug_connection=False)
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
--- docs/source/getting_started/using_xoa_converter.rst
+++ docs/source/getting_started/using_xoa_converter.rst
@@ -9,17 +9,8 @@ First, import ``xoa_core`` and ``xoa_converter`` into your Python code. If you h
     from xoa_converter.entry import converter
     from xoa_converter.types import TestSuiteType
 
-Then, to use the converter, you need the target schema for the old file to be converted into:
-
-.. code-block:: python
-
-    core_ctrl = await controller.MainController() # create an instance of xoa core controller
-    info = core_ctrl.get_test_suite_info("RFC-2544") # get 2544 test suite information from the core's registration
-    target_schema = json.load(info['schema']) # get the target json schema
-
-After that, simply provide the target schema, the old config file to the converter function and get the new config file:
+Then, to use the converter, you need the target schema for the old file to be converted into. After that, simply provide the target schema, the old config file to the converter function and get the new config file:
 
 .. literalinclude:: ../code_example/using_converter.py
     :language: python
-    :linenos:
-    :emphasize-lines: 3, 4, 5, 10, 11, 12, 15
+    :emphasize-lines: 17, 20-30

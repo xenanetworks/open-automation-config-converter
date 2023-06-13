8f4a37e67232aaee02d3f6551e419f5e19e25278
fpfeng <minifen9@gmail.com>
2023-06-13 11:25:48 +0800

    feat: create 1564 base code

--- xoa_converter/converters/fabric.py
+++ xoa_converter/converters/fabric.py
@@ -4,6 +4,7 @@ from xoa_converter import types
 from .rfc2544.adapter import Converter2544
 from .rfc3918.adapter import Converter3918
 from .rfc2889.adapter import Converter2889
+from .y1564.adapter import Converter1564
 
 
 class XoaConverter(Protocol):
@@ -11,12 +12,17 @@ class XoaConverter(Protocol):
     def gen(self) -> Dict[str, Any]: ...
 
 
+CONVERTER_MAPPING: Dict[types.TestSuiteType, Type[XoaConverter]] = {
+    types.TestSuiteType.RFC2544: Converter2544,
+    types.TestSuiteType.RFC3918: Converter3918,
+    types.TestSuiteType.RFC2889: Converter2889,
+    types.TestSuiteType.Y1564: Converter1564,
+}
+
+
 def get_converter(test_suite_type: types.TestSuiteType) -> Type[XoaConverter]:
     """Selecting and returning Converter"""
-    if test_suite_type == types.TestSuiteType.RFC2544:
-        return Converter2544
-    elif test_suite_type == types.TestSuiteType.RFC3918:
-        return Converter3918
-    elif test_suite_type == types.TestSuiteType.RFC2889:
-        return Converter2889
-    raise exceptions.UnknowConverterType(test_suite_type)
+    converter = CONVERTER_MAPPING.get(test_suite_type)
+    if not converter:
+        raise exceptions.UnknowConverterType(test_suite_type)
+    return converter
---
+++ xoa_converter/converters/y1564/adapter.py
@@ -0,0 +1,9 @@
+from typing import Any, Dict
+
+
+class Converter1564:
+    def gen(self) -> Dict[str, Any]:
+        return {}
+
+    def __init__(self, source_config: str) -> None:
+        pass
